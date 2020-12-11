e7878daf7e52fc1e7d2de66d2aca173d467681be
Swaroo-pa <swaroopa.k@inxsasia.com>
2020-12-11 09:48:17 +0530

    login page

---
+++ login/app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.login;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username,password;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        button = findViewById(R.id.submit);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("password")){
+                    Intent i = new Intent(MainActivity.this,next.class);
+                    startActivity(i);
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Invalid Username or Password",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });*/
+    }
+}
